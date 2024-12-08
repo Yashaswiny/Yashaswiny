f09ff5de96f3a9f655cfb38e33ec3a077319dd21
Yashaswiny <22wh1a6607@bvrithyderabad.edu.in>
2024-12-08 18:54:21 +0530

    Create expense.dart

---
+++ lib/models/expense.dart
@@ -0,0 +1,15 @@
+class Expense {
+  final String id;
+  final String account;
+  final String description;
+  final double amount;
+  final DateTime date;
+
+  Expense({
+    required this.id,
+    required this.account,
+    required this.description,
+    required this.amount,
+    required this.date,
+  });
+}
